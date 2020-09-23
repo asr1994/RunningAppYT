dd2874046312ef9819abb35f8c509b906ec5b71e
ali.soleimani <ali.soleimanimysso1@partdp.ir>
2020-09-23 15:57:52 +0330

    Configure Google Mpas in TrackingFragment.kt

--- app/src/main/java/com/androiddevs/runningappyt/ui/fragments/TrackingFragment.kt
+++ app/src/main/java/com/androiddevs/runningappyt/ui/fragments/TrackingFragment.kt
@@ -7,6 +7,7 @@ import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import com.androiddevs.runningappyt.R
 import com.androiddevs.runningappyt.ui.viewmodels.MainViewModel
+import com.google.android.gms.maps.GoogleMap
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.android.synthetic.main.fragment_tracking.*
 
@@ -15,12 +16,48 @@ class TrackingFragment : Fragment(R.layout.fragment_tracking) {
 
     private val viewModel: MainViewModel by viewModels()
 
+    private var map: GoogleMap? = null
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        mapView?.onCreate(savedInstanceState)
+
+        mapView.getMapAsync { map = it }
 
         btnFinishRun.setOnClickListener {
             findNavController().navigate(R.id.action_trackingFragment_to_runFragment)
         }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        mapView?.onStart()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        mapView?.onStop()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        mapView?.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        mapView?.onPause()
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        mapView?.onLowMemory()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        mapView?.onSaveInstanceState(outState)
     }
 
 }
